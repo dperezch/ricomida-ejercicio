54404c0f5273ed58eeeb031686e6130a9e29d39e
dperezch <davarmando@gmail.com>
2024-02-17 15:24:12 -0300

    footer, JQuery functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,26 @@ $(document).ready(function () {
   const tooltipList = [...tooltipTriggerList].map(
     (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
   );
+
+  /* Cambiar color de texto a ingreientes y preparación */
+  $(".ingredients>div>div>h3").dblclick(function () {
+    $(this).addClass("text-danger");
+  });
+
+  $(".ingredients>div>div:last-child>h3").on("dblclick", function () {
+    $(this).addClass("text-danger");
+  });
+
+  /* Añadir toggle a tarjetas */
+  $("#recipe1").click(function () {
+    $("#recipe1-text").toggle();
+  });
+
+  $(".recipe2").click(function () {
+    $(".recipe2-text").toggle("slow", function () {});
+  });
+
+  $(".recipe3").click(function () {
+    $(".recipe3-text").toggle(1500, "swing", function () {});
+  });
 });
