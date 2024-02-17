2f7e53becd09bcd0fb6898c37dd566526c94da99
dperezch <davarmando@gmail.com>
2024-02-16 23:49:06 -0300

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  /* Tooltip Bootstrap */
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  );
+});
